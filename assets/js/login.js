ea0297ea7793be668dbc9b132b302bf1be4f592b
my name is npc <1838596883@qq.com>
2021-10-09 20:07:11 +0800

    做好了登录并修改了一些bug

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,9 +16,38 @@ $(function() {
             /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
         ]
     });
+
     $("#form-reg").on("submit", function(e) {
         e.preventDefault();
-        $.post('http://ajax.frontend.itheima.net/api/reguser', { username: $('#form-reg[name=username]').val(), password: $('#form-reg[name=password]').val() }, function(reg) {})
-        alert('注册成功');
+        $.post('http://ajax.frontend.itheima.net/api/reguser', { username: $('#form-reg input:eq(0)').val(), password: $('#form-reg input:eq(1)').val() }, function(reg) {
+            if (reg.status !== 0) {
+                return console.log(reg.message);
+            }
+        })
+        console.log("注册成功");
+        var lname = $('#form-reg input:eq(0)').val();
+        var lpwd = $('#form-reg input:eq(1)').val();
+        localStorage.setItem('name', lname);
+        localStorage.setItem('pwd', lpwd);
+        console.log(lname);
+        console.log(lpwd);
+        $(".login h5").click();
+    })
+    $("#form-index").on('submit', function(e) {
+        e.preventDefault();
+        console.log("点击了提交");
+        var dname = $("#form-index input:eq(0)").val();
+        var dpwd = $('#form-index input:eq(1)').val();
+        var lname = localStorage.getItem("name");
+        var lpwd = localStorage.getItem("pwd");
+        if (dname === lname && dpwd === lpwd) {
+            location.href = 'index.html';
+        } else {
+            alert('用户名或密码错误');
+        }
+        // console.log(dname);
+        // console.log(dpwd);
+        // console.log(lname);
+        // console.log(lpwd);
     })
 })
